ff91deb1e74da2a60688c700552c26bd1c45ada7
Ubuntu <ubuntu@ip-172-31-39-65.ap-south-1.compute.internal>
2024-06-19 08:50:02 +0000

    first commit

--- examples/getstarted/config/database.js
+++ examples/getstarted/config/database.js
@@ -1,63 +1,14 @@
-const sqlite = {
-  client: 'sqlite',
+module.exports = ({ env }) => ({
   connection: {
-    filename: '.tmp/data.db',
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'root1909'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
   },
-  useNullAsDefault: true,
-};
+});
 
-const postgres = {
-  client: 'postgres',
-  connection: {
-    database: 'strapi',
-    user: 'strapi',
-    password: 'strapi',
-    port: 5432,
-    host: 'localhost',
-  },
-};
-
-const mysql = {
-  client: 'mysql',
-  connection: {
-    database: 'strapi',
-    user: 'strapi',
-    password: 'strapi',
-    port: 3306,
-    host: 'localhost',
-  },
-};
-
-const mysql2 = {
-  client: 'mysql2',
-  connection: {
-    database: 'strapi',
-    user: 'strapi',
-    password: 'strapi',
-    port: 3306,
-    host: 'localhost',
-  },
-};
-
-const mariadb = {
-  client: 'mysql',
-  connection: {
-    database: 'strapi',
-    user: 'strapi',
-    password: 'strapi',
-    port: 3307,
-    host: 'localhost',
-  },
-};
-
-const db = {
-  mysql,
-  mysql2,
-  sqlite,
-  postgres,
-  mariadb,
-};
-
-module.exports = {
-  connection: process.env.DB ? db[process.env.DB] || db.sqlite : db.sqlite,
-};
